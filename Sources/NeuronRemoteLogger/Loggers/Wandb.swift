8f41ea1a29b567ee8297051fae0a056f7447d774
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2023-05-01 22:03:31 -0400

    working on wandb

--- Sources/NeuronRemoteLogger/Loggers/Wandb.swift
+++ Sources/NeuronRemoteLogger/Loggers/Wandb.swift
@@ -6,4 +6,16 @@
 //
 
 import Foundation
+import PythonKit
 
+public class Wandb: RemoteLogger {
+  public var type: Remote = .wandb
+  
+  private var wandb: PythonObject?
+  
+  public init() throws {
+    wandb = try Python.attemptImport("wandb")
+    print(wandb?.login)
+  }
+  
+}
--- Tests/NeuronRemoteLoggerTests/NeuronRemoteLoggerTests.swift
+++ Tests/NeuronRemoteLoggerTests/NeuronRemoteLoggerTests.swift
@@ -2,5 +2,12 @@ import XCTest
 @testable import NeuronRemoteLogger
 
 final class NeuronRemoteLoggerTests: XCTestCase {
-
+  
+  func test_wandbLogin() {
+    do {
+      let wandb = try Wandb()
+    } catch {
+      XCTFail(error.localizedDescription)
+    }
+  }
 }
